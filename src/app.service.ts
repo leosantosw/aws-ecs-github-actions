0132ac164ff4e8fbbd6b83e0b57cbc8549cc5b19
Leonardo Santos <leonardo.nascimento@marche.com.br>
2022-04-21 21:55:54 -0300

    add files

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello() {
+    return JSON.stringify({
+      status: process.env.STATUS,
+      message: process.env.MSG,
+    });
+  }
+}
