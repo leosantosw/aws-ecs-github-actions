e2ef975c22ca860e42ca92372142cbc7b2d1f941
Leonardo Santos <leonardo.nascimento@marche.com.br>
2022-04-22 19:30:07 -0300

    update service

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,7 @@ export class AppService {
     return JSON.stringify({
       status: process.env.STATUS,
       message: process.env.MESSAGE,
+      timestamp: new Date().getTime(),
     });
   }
 }
